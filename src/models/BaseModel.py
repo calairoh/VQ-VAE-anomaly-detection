27bf100e679cd09db6a1d3b0c8a5661dbb4b8288
Davide Calabrò <davide.calabro@mail.polimi.it>
2021-07-12 11:49:17 +0200

    Best model so far with 64x64 images

--- src/models/BaseModel.py
+++ src/models/BaseModel.py
@@ -21,16 +21,16 @@ class ConvVAE(nn.Module):
 
 
         # fully connected layers for learning representations
-        self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(1024, 64)
+        #self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(init_channels * 8, 64)
         self.fc_mu = nn.Linear(64, latent_dim)
         self.fc_log_var = nn.Linear(64, latent_dim)
         self.fc2 = nn.Linear(latent_dim, 64)
 
         # decoder
-        self.dec1 = nn.ConvTranspose2d(64, init_channels * 4, kernel_size=4, stride=1, padding=0)
-        self.dec2 = nn.ConvTranspose2d(init_channels * 4, init_channels * 2, kernel_size=4, stride=2, padding=1)
-        self.dec3 = nn.ConvTranspose2d(init_channels * 2, init_channels, kernel_size=4, stride=2, padding=1)
+        self.dec1 = nn.ConvTranspose2d(64, init_channels * 8, kernel_size=4, stride=1, padding=0)
+        self.dec2 = nn.ConvTranspose2d(init_channels * 8, init_channels * 4, kernel_size=4, stride=2, padding=1)
+        self.dec3 = nn.ConvTranspose2d(init_channels * 4, init_channels * 2, kernel_size=4, stride=2, padding=1)
         self.dec4 = nn.ConvTranspose2d(init_channels * 2, init_channels, kernel_size=4, stride=2, padding=1)
         self.dec5 = nn.ConvTranspose2d(init_channels, image_channels, kernel_size=4, stride=2, padding=1)
 
@@ -55,8 +55,7 @@ class ConvVAE(nn.Module):
         x = F.relu(self.enc4(x))
 
         batch, _, _, _ = x.shape
-        #x = F.adaptive_avg_pool2d(x, 1).reshape(batch, -1)
-        x = self.flatten(x)
+        x = F.adaptive_avg_pool2d(x, 1).reshape(batch, -1)
         hidden = self.fc1(x)
         # get `mu` and `log_var`
         mu = self.fc_mu(hidden)
