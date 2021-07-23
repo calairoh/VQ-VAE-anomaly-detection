93b92756ed0587812490ad4472326bd9c4c7b2c0
Davide Calabrò <davide.calabro@mail.polimi.it>
2021-07-23 11:24:13 +0200

    Add classify function

--- src/performance/classification.py
+++ src/performance/classification.py
@@ -1,10 +1,10 @@
 from tqdm import tqdm
 
 from src.engine import final_loss
-from src.metrics.classification import accuracy, recall, precision
+from src.metrics.classification import accuracy, recall, precision, tpr, fpr
 
 
-def classification_test(net, testloader, testset, device, criterion, threshold):
+def classification_test(net, testloader, testset, device, criterion, thresholds):
     res = []
     for i, data in tqdm(enumerate(testloader), total=len(testset)):
         data = data['image']
@@ -14,14 +14,28 @@ def classification_test(net, testloader, testset, device, criterion, threshold):
         loss = final_loss(bce_loss, mu, logvar)
         loss.backward()
 
-        label = 0 if loss < threshold else 1
+        res.append({'loss': loss, 'realLabel': data['label']})
 
-        res.append({'idx': i, 'label': label, 'realLabel': data['label']})
+    for t in thresholds:
+        classify(res, t)
 
-    acc = accuracy(res)
-    pre = precision(res)
-    rec = recall(res)
 
+def classify(res, threshold):
+    data = []
+    for i, r in res:
+        label = 0 if r['loss'] < threshold else 1
+        data.append({'label': label, 'realLabel': data['label']})
+
+    acc = accuracy(data)
+    pre = precision(data)
+    rec = recall(data)
+    tp_rate = tpr(data)
+    fp_rate = fpr(data)
+
+    print('-----------------------------')
+    print('Threshold: ' + threshold)
     print('Accuracy: ' + acc)
     print('Precision: ' + pre)
     print('Recall: ' + rec)
+    print('TPR: ' + tp_rate)
+    print('FPR' + fp_rate)
