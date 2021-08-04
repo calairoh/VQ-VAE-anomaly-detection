3c1a108496e8fbcf4971725514678e031322b1a7
Davide Calabrò <davide.calabro@mail.polimi.it>
2021-08-04 14:50:44 +0200

    Add RGGB enum + keepOnly function

--- src/image/ImageElaboration.py
+++ src/image/ImageElaboration.py
@@ -1,3 +1,6 @@
 class ImageElaboration:
     def __init__(self, image):
         self.image = image
+
+    def keep_only(self, rgb):
+        return self.image[:, :, int(rgb) - 1]
---
+++ src/image/RGB.py
@@ -0,0 +1,7 @@
+import enum
+
+
+class RGB(enum.Enum):
+    RED = 1
+    GREEN = 2
+    BLU = 3
